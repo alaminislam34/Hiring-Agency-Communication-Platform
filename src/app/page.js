fa2aab6f852f516b625048801294b4c438348949
alaminislam34 <mi3548514@gmail.com>
2025-03-12 19:03:55 +0600

    added alamin component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import JobCandidate from "./components/JobCandidate";
+import Alamin from "./components/Alamin";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
           Button Color
         </button>
       </div>
+      <Alamin />
     </div>
   );
 }

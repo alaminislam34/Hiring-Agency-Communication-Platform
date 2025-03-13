aec47b73e39f6bff74fc5e3c399a7fc09d171a74
alaminislam34 <mi3548514@gmail.com>
2025-03-13 10:54:43 +0600

    added seekkit section component

---
+++ src/app/components/SeekKit.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SeekKit = () => {
+  return (
+    <div>
+      <h1 className="text-2xl md:text-3xl lg:text-4xl text-center py-6">
+        Job seeker tool kit.
+      </h1>
+    </div>
+  );
+};
+
+export default SeekKit;

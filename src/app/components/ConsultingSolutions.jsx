ea263b4c8dba830b50ee4361173735b665e99222
alaminislam34 <mi3548514@gmail.com>
2025-03-14 15:41:10 +0600

    updated: some section design update

--- src/app/components/ConsultingSolutions.jsx
+++ src/app/components/ConsultingSolutions.jsx
@@ -3,10 +3,9 @@ import { CheckCircleIcon } from "@heroicons/react/24/solid";
 
 export default function ConsultingSolutions() {
   return (
-    <div className="flex items-center justify-center  bg-gray-100 px-6">
-      <div className="bg-white shadow-lg rounded-xl flex items-center   gap-10">
-        
-        <div className="w-1/2">
+    <div className="flex items-center justify-center">
+      <div className="shadow-lg rounded-xl flex items-center flex-col lg:flex-row gap-8 lg:gap-12">
+        <div className="lg:w-1/2">
           <h2 className="text-3xl font-semibold text-gray-900 leading-snug">
             Consulting solutions to drive business impact
           </h2>
@@ -17,7 +16,10 @@ export default function ConsultingSolutions() {
             </li>
             <li className="flex items-start gap-3">
               <CheckCircleIcon className="w-6 h-6 text-green-500" />
-              <span>Enhance business performance with critical technologies and processes</span>
+              <span>
+                Enhance business performance with critical technologies and
+                processes
+              </span>
             </li>
             <li className="flex items-start gap-3">
               <CheckCircleIcon className="w-6 h-6 text-green-500" />
@@ -25,7 +27,6 @@ export default function ConsultingSolutions() {
             </li>
           </ul>
 
-          
           <div className="mt-6 flex gap-4">
             <button className="bg-green-900 text-white px-6 py-3 rounded-full text-lg font-medium hover:bg-green-800 transition">
               Find your consulting solution
@@ -37,12 +38,12 @@ export default function ConsultingSolutions() {
         </div>
 
         <Image
-            src="/consulting.webp" 
-            alt="Business meeting"
-            width={500}
-            height={400}
-            className="rounded-lg shadow-md"
-          />
+          src="/consulting.webp"
+          alt="Business meeting"
+          width={500}
+          height={400}
+          className="rounded-lg shadow-md"
+        />
       </div>
     </div>
   );

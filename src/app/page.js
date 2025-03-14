e75fef7948998502a02d4713163bc35f465e817c
Mohammed Rakib Gazi <gazicourses@gmail.com>
2025-03-14 11:51:56 +0600

    git conflictt resolved

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,23 @@
-// import Image from "next/image";
-
+import Accordion from "./components/Accordion";
 import Banner from "./components/Banner";
 import CareerMove from "./components/CareerMove";
 import ConsultingSolutions from "./components/ConsultingSolutions";
 import FindJob from "./components/FindJob";
 import FindTalent from "./components/FindTalent";
 import SeekKit from "./components/SeekKit";
+import Testimonials from "./components/Testimonials";
 
 export default function Home() {
   return (
-    <div className="pt-10">
-      <Banner></Banner>
-      <FindJob></FindJob>
+    <div>
+      <Banner />
+      <FindJob />
       <SeekKit />
-      <FindTalent/>
-      <ConsultingSolutions/>
-      <CareerMove/>
+      <FindTalent />
+      <ConsultingSolutions />
+      <CareerMove />
+      <Testimonials />
+      <Accordion />
     </div>
   );
 }

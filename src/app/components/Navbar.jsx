9404d7afa522149a7a8b36c4c43df9015da4ca48
AkashMAbrrar <madborakash48@gmail.com>
2025-03-23 20:04:01 +0600

    this commit for nothing

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const session = useSession();
   const pathname = usePathname();
   console.log(session);
-  const navlink = [
+  const navLinks = [
     { href: "/", name: "Home" },
     { href: "/jobs", name: "Jobs" },
     { href: "/about", name: "About Us" },
@@ -27,15 +27,13 @@ const Navbar = () => {
             <div className="dropdown lg:hidden">
               <label
                 tabIndex={0}
-                className="btn btn-ghost flex items-center gap-2"
-              >
+                className="btn btn-ghost flex items-center gap-2">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
+                  stroke="currentColor">
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
@@ -47,9 +45,12 @@ const Navbar = () => {
               {/* Mobile Dropdown Menu */}
               <ul
                 tabIndex={0}
-                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                {navLinks}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                {navLinks.map(({ href, name }) => (
+                  <li key={href}>
+                    <Link href={href}>{name}</Link>
+                  </li>
+                ))}
               </ul>
             </div>
 
@@ -62,13 +63,12 @@ const Navbar = () => {
           {/* Navbar Center (Desktop Menu) */}
           <div className="navbar-center hidden lg:flex">
             <ul className="flex flex-row gap-6 text-md">
-              {navlink.map(({ href, name }) => (
+              {navLinks.map(({ href, name }) => (
                 <li
                   key={href}
                   className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#013005] after:transition-all after:duration-300 hover:after:w-full ${
                     pathname === href ? "after:w-full" : ""
-                  } py-2 px-2`}
-                >
+                  } py-2 px-2`}>
                   <Link href={href} className="">
                     {name}
                   </Link>
@@ -86,8 +86,7 @@ const Navbar = () => {
                 className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
+                stroke="currentColor">
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
@@ -112,8 +111,7 @@ const Navbar = () => {
               <div className="flex items-center gap-2">
                 <Link
                   href="/signin"
-                  className="btn btn-outline btn-accent btn-sm md:btn-md"
-                >
+                  className="btn btn-outline btn-accent btn-sm md:btn-md">
                   Sign In
                 </Link>
               </div>

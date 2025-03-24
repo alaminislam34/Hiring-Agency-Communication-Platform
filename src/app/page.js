b67ba5a0ac0f845ee87a9bb48eda9d75248fd1ad
alaminislam34 <mi3548514@gmail.com>
2025-03-24 10:35:13 +0600

    updated some design on the jobscard component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,4 @@
 import { ToastContainer } from "react-toastify";
-// import Image from "next/image";
-
-import { ToastContainer } from "react-toastify";
-import Chat from "./chatbox/components/chat";
-import ChatPage from "./chatbox/components/ChatPage";
 import Accordion from "./components/Accordion";
 import Banner from "./components/Banner";
 import CareerMove from "./components/CareerMove";
@@ -26,8 +21,6 @@ export default async function Home() {
       <CareerMove />
       <Testimonials />
       <Accordion />
-
-      <ToastContainer position="top-center" autoClose={3000} />
       <ToastContainer position="top-center" autoClose={3000} />
     </div>
   );

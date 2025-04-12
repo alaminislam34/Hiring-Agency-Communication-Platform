64c3f3027e15f895aada4a731fff59a70349ff1a
alaminislam34 <mi3548514@gmail.com>
2025-04-12 10:13:57 +0600

    updated navbar design

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,12 +1,15 @@
 "use client";
 import { signOut } from "next-auth/react";
-import React from "react";
+import { FiLogOut } from "react-icons/fi";
 
 const LogoutButton = () => {
   return (
     <div>
-      <button onClick={() => signOut()} className="btn btn-accent">
-        Logout
+      <button
+        onClick={() => signOut()}
+        className="flex items-center gap-2 cursor-pointer"
+      >
+        <FiLogOut /> Logout
       </button>
     </div>
   );

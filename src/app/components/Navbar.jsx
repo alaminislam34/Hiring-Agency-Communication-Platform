b8a49823ec9395a7c15033d3361954dcb57b6638
alaminislam34 <mi3548514@gmail.com>
2025-04-13 19:35:04 +0600

    added navbar issue

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,11 +1,14 @@
 "use client";
 import LoginButton from "@/components/LoginButton";
 import LogoutButton from "@/components/LogoutButton";
-
 import { useAppContext } from "@/Providers/AppProviders";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
+import { HiOutlineUserCircle } from "react-icons/hi2";
+import { TbLayoutDashboard } from "react-icons/tb";
+import { VscSaveAll } from "react-icons/vsc";
+import { CiBookmarkCheck } from "react-icons/ci";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { FaCog, FaQuestionCircle } from "react-icons/fa";
 import { FaBriefcase, FaUsers } from "react-icons/fa6";
@@ -18,25 +21,185 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const { currentUser } = useAppContext();
+  const jobSeekerNavLink = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Jobs",
+      href: "/jobs",
+    },
+    {
+      name: "Saved Jobs",
+      href: "/savedJobs",
+    },
+    {
+      name: "Applied Jobs",
+      href: "/appliedJobs",
+    },
+    {
+      name: "Blogs",
+      href: "/blogs",
+    },
+  ];
+  const NavLinks = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Jobs",
+      href: "/jobs",
+    },
+    {
+      name: "Saved Jobs",
+      href: "/savedJobs",
+    },
 
-  const navLink = [
-    { href: "/", name: "Home" },
-    { href: "/jobs", name: "Jobs" },
-    { href: "/about", name: "About Us" },
-    { href: "/blogs", name: "Blogs" },
+    {
+      name: "Blogs",
+      href: "/blogs",
+    },
+  ];
+  const adminNavLinks = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Jobs",
+      href: "/jobs",
+    },
+    {
+      name: "About Us",
+      href: "/about",
+    },
+    {
+      name: "Reports",
+      href: "/adminReportsPage",
+    },
+    {
+      name: "Manage Users",
+      href: "/manageUsers",
+    },
+    {
+      name: "Manage Jobs",
+      href: "/manageJobs",
+    },
+  ];
+  const employerNavLinks = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Jobs",
+      href: "/jobs",
+    },
+    {
+      name: "Post Job",
+      href: "/postJob",
+    },
+    {
+      name: "Reports",
+      href: "/employerReportsPage",
+    },
+    {
+      name: "Blogs",
+      href: "/blogs",
+    },
   ];
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (searchRef.current && !searchRef.current.contains(event.target)) {
-        setIsOpen(false);
-      }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+  // jobSeeker links
+  const jobSeekerLinks = [
+    {
+      name: "Profile",
+      href: "/dashboard/profile",
+      icon: <HiOutlineUserCircle />,
+    },
+    {
+      name: "Dashboard",
+      href: "/dashboard",
+      icon: <TbLayoutDashboard />,
+    },
+    {
+      name: "Saved Jobs",
+      href: "/dashboard/saved-jobs",
+      icon: <VscSaveAll />,
+    },
+    {
+      name: "Applied Jobs",
+      href: "/dashboard/applied-jobs",
+      icon: <CiBookmarkCheck />,
+    },
+    {
+      name: "Notifications",
+      href: "/dashboard/notifications",
+      icon: <IoIosNotificationsOutline />,
+    },
+  ];
+  // employer links
+  const employerLinks = [
+    {
+      name: "Profile",
+      href: "/dashboard/profile",
+      icon: <HiOutlineUserCircle />,
+    },
+    {
+      name: "Dashboard",
+      href: "/dashboard",
+      icon: <TbLayoutDashboard />,
+    },
+    {
+      name: "Jobs",
+      href: "/dashboard/jobs",
+      icon: <FaBriefcase />,
+    },
+    {
+      name: "Candidates",
+      href: "/dashboard/candidates",
+      icon: <FaUsers />,
+    },
+    {
+      name: "Settings",
+      href: "/dashboard/settings",
+      icon: <FaCog />,
+    },
+    {
+      name: "Help",
+      href: "/dashboard/help",
+      icon: <FaQuestionCircle />,
+    },
+  ];
+  // admin links
+  const adminLinks = [
+    {
+      name: "Profile",
+      href: "/dashboard/profile",
+      icon: <HiOutlineUserCircle />,
+    },
+    {
+      name: "Admin Panel",
+      href: "/dashboard",
+      icon: <TbLayoutDashboard />,
+    },
+    {
+      name: "Users",
+      href: "/dashboard/users",
+      icon: <FaUsers />,
+    },
+    {
+      name: "Jobs",
+      href: "/dashboard/jobs",
+      icon: <FaBriefcase />,
+    },
+    {
+      name: "Settings",
+      href: "/dashboard/settings",
+      icon: <FaCog />,
+    },
+  ];
 
   return (
     <div className="bg-base-100 shadow-md h-[65px] relative">
@@ -136,16 +299,57 @@ const Navbar = () => {
           {/* Navbar Center (Desktop Menu) */}
           <div className="navbar-center hidden lg:flex">
             <ul className="flex flex-row gap-6 text-md">
-              {navLink.map(({ href, name }) => (
-                <li
-                  key={href}
-                  className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-gray-500 after:transition-all after:duration-300 hover:after:w-full ${
-                    pathname === href ? "after:w-full" : ""
-                  } py-1 px-2`}
-                >
-                  <Link href={href}>{name}</Link>
-                </li>
-              ))}
+              {currentUser?.role === "jobSeeker"
+                ? jobSeekerNavLinks.map(({ href, name }) => (
+                    <li
+                      key={href}
+                      className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-teal-500 after:transition-all after:duration-300 hover:after:w-full ${
+                        pathname === href ? "after:w-full" : ""
+                      } py-1 px-2`}
+                    >
+                      <Link href={href} className="">
+                        {name}
+                      </Link>
+                    </li>
+                  ))
+                : currentUser?.role === "employer"
+                ? employerNavLinks.map(({ href, name }) => (
+                    <li
+                      key={href}
+                      className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-teal-500 after:transition-all after:duration-300 hover:after:w-full ${
+                        pathname === href ? "after:w-full" : ""
+                      } py-1 px-2`}
+                    >
+                      <Link href={href} className="">
+                        {name}
+                      </Link>
+                    </li>
+                  ))
+                : currentUser?.role === "admin"
+                ? adminNavLinks.map(({ href, name }) => (
+                    <li
+                      key={href}
+                      className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-teal-500 after:transition-all after:duration-300 hover:after:w-full ${
+                        pathname === href ? "after:w-full" : ""
+                      } py-1 px-2`}
+                    >
+                      <Link href={href} className="">
+                        {name}
+                      </Link>
+                    </li>
+                  ))
+                : NavLinks.map(({ href, name }) => (
+                    <li
+                      key={href}
+                      className={`relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-teal-500 after:transition-all after:duration-300 hover:after:w-full ${
+                        pathname === href ? "after:w-full" : ""
+                      } py-1 px-2`}
+                    >
+                      <Link href={href} className="">
+                        {name}
+                      </Link>
+                    </li>
+                  ))}
             </ul>
           </div>
 
@@ -154,13 +358,12 @@ const Navbar = () => {
             <div>
               <Link
                 href="/dashboard/notifications"
-                className="flex items-center justify-center text-2xl hover:text-[#00e1ff] cursor-pointer"
+                className="flex items-center justify-center text-2xl hover:text-teal-500 cursor-pointer"
               >
                 <IoIosNotificationsOutline />
               </Link>
             </div>
 
-            {/* Profile Dropdown */}
             {currentUser ? (
               <div className="flex items-center gap-2 relative">
                 <img
@@ -173,71 +376,53 @@ const Navbar = () => {
                   className={`absolute top-[65px] right-0 z-50 overflow-hidden bg-white border border-gray-300 shadow-lg rounded-xl w-64 duration-300 transition-all ${
                     isDropdownOpen
                       ? "opacity-100 scale-100"
-                      : "opacity-0 scale-95 pointer-events-none"
+                      : "opacity-0 scale-50 pointer-events-none"
                   }`}
                 >
-                  {/* Dropdown Menu */}
-                  <ul className="space-y-3 text-gray-700">
-                    <li className="font-semibold">{currentUser?.name}</li>
-                    <li className="text-sm text-gray-500">
-                      {currentUser?.email}
+                  <ul className=" text-gray-700">
+                    <li className="p-4">
+                      <p className="font-semibold">{currentUser?.name}</p>
+                      <p className="text-sm text-gray-500">
+                        {currentUser?.email}
+                      </p>
                     </li>
-                    <hr />
-                    {currentUser?.role === "jobSeeker" && (
-                      <>
-                        <li>
-                          <Link
-                            href="/employerDashboard/profile"
-                            className="block hover:text-primary"
-                          >
-                            Profile
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="/dashboard"
-                            className="block hover:text-primary"
-                          >
-                            Dashboard
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href="#" className="block hover:text-primary">
-                            Saved Jobs
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href="#" className="block hover:text-primary">
-                            Applied Jobs
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href="#" className="block hover:text-primary">
-                            Notifications
-                          </Link>
-                        </li>
-                      </>
-                    )}
-                    {currentUser?.role === "employer" && (
-                      <li>
-                        <Link
-                          href="/dashboard"
-                          className="block hover:text-primary"
+                    <li className="border-b border-gray-300"></li>
+                    {/* Job Seeker Menu */}
+                    {currentUser?.role === "jobSeeker" &&
+                      jobSeekerLinks.map(({ name, href, icon }) => (
+                        <li
+                          key={name}
+                          className="hover:bg-[#cbfeff] text-[#105269]/80 hover:text-[#033649] px-3 py-2 duration-300 transition-all cursor-pointer "
                         >
-                          Dashboard
-                        </Link>
-                      </li>
-                    )}
-                    {currentUser?.role === "admin" && (
-                      <li>
-                        <Link
-                          href="/dashboard"
-                          className="block hover:text-primary"
+                          <Link href={href} className="flex items-center gap-2">
+                            {icon} {name}
+                          </Link>
+                        </li>
+                      ))}
+                    {/* Employer Menu */}
+                    {currentUser?.role === "employer" &&
+                      employerLinks.map(({ name, href, icon }) => (
+                        <li
+                          key={name}
+                          className="hover:bg-[#cbfeff] text-[#105269]/80 hover:text-[#033649] px-3 py-2 duration-300 transition-all cursor-pointer "
                         >
-                          Admin Panel
-                        </Link>
-                      </li>
-                    )}
+                          <Link href={href} className="flex items-center gap-2">
+                            {icon} {name}
+                          </Link>
+                        </li>
+                      ))}
+                    {/* Admin Menu */}
+                    {currentUser?.role === "admin" &&
+                      adminLinks.map(({ name, href, icon }) => (
+                        <li
+                          key={name}
+                          className="hover:bg-[#cbfeff] text-[#105269]/80 hover:text-[#033649] px-3 py-2 duration-300 transition-all cursor-pointer "
+                        >
+                          <Link href={href} className="flex items-center gap-2">
+                            {icon} {name}
+                          </Link>
+                        </li>
+                      ))}
                     <li className="border-b border-gray-300"></li>
                     <li className="py-2 px-3 hover:bg-red-300 hover:text-black duration-300 cursor-pointer">
                       <LogoutButton />
